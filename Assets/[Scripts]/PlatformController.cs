4fd1e4d146e3ff5ee5ee1495c887aa19399e3ee8
MSI\zackt <ctse48@my.centennialcollege.ca>
2023-01-23 18:02:31 -0500

    Platform Prefabs

---
+++ Assets/[Scripts]/PlatformController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+  
+  private void OnCollisionEnter2D(Collision2D other) {
+    if(other.gameObject.CompareTag("Player")){
+      other.transform.SetParent(transform);
+    }
+  }
+
+  private void OnCollisionExit2D(Collision2D other) {
+    if(other.gameObject.CompareTag("Player")){
+      other.transform.SetParent(null);
+    }
+  }
+}
