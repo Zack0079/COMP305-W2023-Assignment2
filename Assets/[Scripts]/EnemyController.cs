043913c0979941b07d3ad3b1f219d157150e082e
MSI\zackt <ctse48@my.centennialcollege.ca>
2023-02-20 07:48:21 -0500

    Added RnrmyController v2

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -9,30 +9,58 @@ public class EnemyController : MonoBehaviour
   public float horizontalSpeed = 1.0f;
   public Transform inFrontCheck;
   public Transform groundAheadCheck;
-  public Transform GroundPoint;
+  public Transform groundPoint;
   public float groundRadius;
   public LayerMask groundLayerMask;
   public bool isObstracleInFront;
   public bool isGrounded;
   public bool isGroundAhead;
+  public Vector2 direction;
 
   // Start is called before the first frame update
   void Start()
   {
+    direction = Vector2.left;
+
     rigidbody2D = GetComponent<Rigidbody2D>();
   }
 
   // Update is called once per frame
   void Update()
   {
+    isObstracleInFront = Physics2D.Linecast(groundPoint.position, inFrontCheck.position, groundLayerMask);
+    isGroundAhead = Physics2D.Linecast(groundPoint.position, groundAheadCheck.position, groundLayerMask);
+    isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundRadius, groundLayerMask);
 
+    if (isGroundAhead)
+    {
+      Move();
+    }
+    if (!isGroundAhead || isObstracleInFront)
+    {
+      Flip();
+    }
+  }
+
+  private void Move()
+  {
+    transform.position += new Vector3(direction.x * horizontalSpeed * Time.deltaTime, 0.0f);
+  }
+
+
+
+  private void Flip()
+  {
+    var x = transform.localScale.x * -1.0f;
+    direction *= -1.0f;
+    transform.localScale = new Vector3(x, 1, 1);
   }
 
   private void OnDrawGizmos()
   {
     Gizmos.color = Color.cyan;
-    Gizmos.DrawWireSphere(GroundPoint.position, groundRadius);
-    Gizmos.DrawLine(GroundPoint.position, inFrontCheck.position);
-    Gizmos.DrawLine(GroundPoint.position, groundAheadCheck.position);
+    Gizmos.DrawWireSphere(groundPoint.position, groundRadius);
+    Gizmos.DrawLine(groundPoint.position, inFrontCheck.position);
+    Gizmos.DrawLine(groundPoint.position, groundAheadCheck.position);
   }
 }
