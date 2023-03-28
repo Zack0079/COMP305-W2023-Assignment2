17d4906b19dd4bbbd6f96f40b5f06424a0d1a982
Zack0079 <113079573+Zack0079@users.noreply.github.com>
2023-03-28 02:41:53 -0400

    Create new font assets and start scene v1

--- Assets/[Scripts]/UI/UIController.cs
+++ Assets/[Scripts]/UI/UIController.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-
   public GameObject miniMap;
-    // Start is called before the first frame update
-    void Start()
+  public TMP_Text startButtonLabel;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (miniMap != null)
     {
-        
+      if (Input.GetKeyDown(KeyCode.M))
+      {
+        miniMap.SetActive(!miniMap.activeInHierarchy);
+      }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.M)){
-          miniMap.SetActive(!miniMap.activeInHierarchy);
-        }
-    }
+  }
+
+  public void OnStartButton_Press()
+  {
+    SceneManager.LoadScene("Main");
+  }
+
+  public void OnStartButton_Down()
+  {
+    startButtonLabel.rectTransform.localPosition = new Vector3(0.0f, -6.0f, 0.0f);
+  }
+
+  public void OnStartButton_Up()
+  {
+    startButtonLabel.rectTransform.localPosition = new Vector3(0.0f, 6.0f, 0.0f);
+  }
+
 }
